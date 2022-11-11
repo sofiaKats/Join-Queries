646170eacd6bf1c92a1b5afc5436b8142e9e2f10
sofiaKats <sophiakatsaouni@gmail.com>
2022-11-12 00:56:43 +0200

    addition of query class

--- src/parser.h
+++ src/parser.h
@@ -4,13 +4,20 @@
 
 using namespace std;
 
-class Parser {
+class Query {
     char* relation[9];
 public:
-    Parser();
-    ~Parser();
-    void OpenFileAndParse(); //opens small.work and reads file line by line, extracting queries
+    Query();
+    ~Query();
     void ParseRelations(char* relations); // parses first part of query to find the relations of the query
     void ParsePredicates(char* predicates); // parses second part of query to find predicates of the query
     void ParseProjections(char* projections); // parses third part of query to find projections of query
+};
+
+class Parser {
+    Query** queries;
+public:
+    Parser();
+    ~Parser();
+    void OpenFileAndParse(); //opens small.work and reads the file line by line, extracting queries
 };
