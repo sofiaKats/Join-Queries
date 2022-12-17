16c6cee0fb8db2da1228ee802a7d0ac0ada69604
sofiaKats <sophiakatsaouni@gmail.com>
2022-12-18 01:13:50 +0200

    addition of metadata, functions fully implemented

--- src/Structures/Relation/Relation.cpp
+++ src/Structures/Relation/Relation.cpp
@@ -7,7 +7,8 @@
 //---------------------------------------------------------------------------
 using namespace std;
 //---------------------------------------------------------------------------
-
+//-                        Relation Functions                               -
+//---------------------------------------------------------------------------
 void Relation::loadRelation(const char* fileName)
 {
   int fd = open(fileName, O_RDONLY);
@@ -46,21 +47,125 @@ void Relation::loadRelation(const char* fileName)
   }
   close(fd);
 }
+
 //---------------------------------------------------------------------------
 Relation::Relation(const char* fileName, int id) : ownsMemory(false)
   // Constructor that loads relation from disk
 {
   loadRelation(fileName);
   this->id = id;
+  column_metadata = new Metadata*[numColumns];
+  for(int colId=0; colId<numColumns; colId++) column_metadata[colId] = new Metadata(this->size);
+  findMINValueOfColumns();
+  findMAXValueOfColumns();
+  // for each column struct, allocate memory for distinct array
+  // and fill it with values so that we find the value of d
+  for(int colId=0; colId<numColumns; colId++) column_metadata[colId]->InitDistinctArr();
+  findDistinctValues();
 }
 //---------------------------------------------------------------------------
 Relation::~Relation()
   // Destructor
 {
-  //for (int i = 0; i<size; i++){
-  //  delete[] columns[i];
-  //}
-  delete[] columns;
+  for(int colId=0; colId<numColumns; colId++){ 
+    delete column_metadata[colId];
+    // delete [] columns[colId];
+  }
+  delete [] column_metadata;
+  delete [] columns;
 }
 
 int Relation::getId(){return id;}
+
+//---------------------------------------------------------------------------
+void Relation::findMINValueOfColumns(void){
+  // for each column there is
+  for(int colId=0; colId<numColumns; colId++) {
+    // find the minimum value of the column,and set l value to min
+    unsigned long int min = columns[colId][0];
+
+    for(int rowId=1; rowId<size; rowId++) 
+      if(columns[colId][rowId] < min) min = columns[colId][rowId];
+    
+    column_metadata[colId]->setL(min);
+    cout << "min value of cold: " << colId << " is " << min << endl;
+  }
+}
+
+//---------------------------------------------------------------------------
+void Relation::findMAXValueOfColumns(void){
+  // for each column there is
+  for(int colId=0; colId<numColumns; colId++) {
+    // find the maximum value of the column,and set u value to max
+    unsigned long int max = columns[colId][0];
+
+    for(int rowId=1; rowId<size; rowId++) 
+      if(columns[colId][rowId] > max) max = columns[colId][rowId];
+    
+    column_metadata[colId]->setU(max);
+    cout << "max value of cold: " << colId << " is " << max << endl;
+  }
+}
+
+//---------------------------------------------------------------------------
+void Relation::findDistinctValues(void) {
+  unsigned long int position;
+  for(int colId=0; colId<numColumns; colId++) {
+    for(int rowId=1; rowId<size; rowId++) {
+
+      if(column_metadata[colId]->getSizeOfDistinctArray() < NVALUE)
+      // if array size < N, we change index ~ x-u ~
+        position = (columns[colId][rowId] > column_metadata[colId]->getU()) ? columns[colId][rowId] - column_metadata[colId]->getU() : column_metadata[colId]->getU() - columns[colId][rowId];
+      else 
+        // if array size >= N, the we change index ~ x-l mod N ~
+        position = (columns[colId][rowId] > column_metadata[colId]->getL()) ? (columns[colId][rowId] - column_metadata[colId]->getL()) % NVALUE : (column_metadata[colId]->getL() - columns[colId][rowId]) % NVALUE;
+
+      //we are looking for distinct values only, if the index is false we increase d by 1
+      if(column_metadata[colId]->distinct_arr[position] != true) {
+        column_metadata[colId]->distinct_arr[position] = true;
+        column_metadata[colId]->increase_d_value_by_1();
+      }
+    }
+    cout << "d value of colId: " << colId << " is " << column_metadata[colId]->getD() << endl;
+  }
+}
+
+//---------------------------------------------------------------------------
+//-                        Metadata Functions                               -
+//---------------------------------------------------------------------------
+
+Metadata::Metadata(uint64_t size):l(0), u(0), f(size), d(-1) { /*cout << "f is: " << f << endl;*/ }
+
+Metadata::~Metadata() { delete [] distinct_arr; }
+
+// returns mininmum value of a particular column
+uint64_t Metadata::getL(void){ return l; }
+
+// sets mininmum value of a particular column
+void Metadata::setL(uint64_t L){ l=L; }
+
+// returns maximum value of a particular column
+uint64_t Metadata::getU(void){ return u; }
+
+// sets maxium value of a particular column
+void Metadata::setU(uint64_t U){ u=U; }
+
+void Metadata::InitDistinctArr(void) {
+  size = u-l+1;
+  if(size > NVALUE) size = NVALUE;
+  distinct_arr = new bool[size];
+  // initialize array with zeros
+  for(int i=0; i<size; i++) distinct_arr[i] = false;
+}
+
+int Metadata::getSizeOfDistinctArray(void){ return size; }
+
+// set the amount of distincted values of column
+void Metadata::setD(int D){ d=D; }
+
+// get the amount of distincted values of column
+int Metadata::getD(void){ return d; }
+
+void Metadata::increase_d_value_by_1(void){ d+=1; }
+
+void Metadata::decrease_d_value_by_1(void){ d-=1; }
--- src/Structures/Relation/Relation.hpp
+++ src/Structures/Relation/Relation.hpp
@@ -1,7 +1,34 @@
 #pragma once
 #include <cstdint>
 #include <unistd.h>
+#include <cstdlib>
 
+#define NVALUE 1000000 // any number less than  50.000.000
+
+//---------------------------------------------------------------------------
+// this is where we store {lΑ,uA,fA,dA} values
+class Metadata {
+private:
+  unsigned long int l;  // minimum value in column
+  unsigned long int u;  // maximum value in column
+  int f;                // number of values in column
+  int d;                // number of distinct values in column
+  int size;             // size of distinct_arr
+public:
+  bool* distinct_arr;    // array used to find d value faster
+  Metadata(uint64_t size);
+  ~Metadata();
+  uint64_t getL(void);
+  void setL(uint64_t L);
+  uint64_t getU(void);
+  void setU(uint64_t U);
+  void InitDistinctArr(void);
+  int getSizeOfDistinctArray(void);
+  void setD(int D);
+  int getD(void);
+  void increase_d_value_by_1(void);
+  void decrease_d_value_by_1(void);
+};
 
 using RelationId = unsigned;
 //---------------------------------------------------------------------------
@@ -21,6 +48,7 @@ class Relation {
   uint64_t numColumns;
   /// The join column containing the keys
   uint64_t** columns;
+  Metadata** column_metadata; // each column has a Metadata struct of it's own. 
   /// Constructor using mmap
   Relation(const char* fileName, int id);
   /// Delete copy constructor
@@ -31,4 +59,8 @@ class Relation {
   ~Relation();
   
   int getId();
+
+  void findMINValueOfColumns(void);
+  void findMAXValueOfColumns(void);
+  void findDistinctValues(void);
 };
