bbff4092305fb16cf9bab259017dccb417f358f0
spyroskantas <proxysinged0@gmail.com>
2022-12-19 13:12:19 +0200

    JobScheduler wait_all_tasks_finish.

--- src/Multithreading/JobScheduler.cpp
+++ src/Multithreading/JobScheduler.cpp
@@ -5,22 +5,27 @@ int JobScheduler::initialize_scheduler(int execution_threads){
   this->execution_threads = execution_threads;
   this->q = new Queue();
   this->quit = false;
-  tids = (pthread_t*)malloc(sizeof(pthread_t) * execution_threads);
+  this->wait_all = false;
+  tids = new pthread_t[execution_threads];
   //initialize mutex and condition variables.
   if(pthread_mutex_init(&qlock,NULL)){
     fprintf(stderr, "[JobScheduler] Mutex initiation error\n");
-	   return -1;
+    return -1;
   }
-  if(pthread_cond_init(&q_empty,NULL)){
+  if(pthread_cond_init(&q_empty, NULL)){
     fprintf(stderr, "[JobScheduler] CV initiation error\n");
-	   return -1;
+    return -1;
   }
-  if(pthread_cond_init(&q_not_empty,NULL)){
+  if(pthread_cond_init(&q_not_empty, NULL)){
     fprintf(stderr, "[JobScheduler] CV initiation error\n");
-	   return -1;
+    return -1;
+  }
+  if(pthread_barrier_init(&barrier, NULL, execution_threads + 1)){
+    fprintf(stderr, "[JobScheduler] Barrier initiation error\n");
+    return -1;
   }
   for (int i=0; i<execution_threads; i++) {
-    if(pthread_create(&(tids[i]),NULL,do_work,this)){
+    if(pthread_create(&(tids[i]), NULL, do_work, this)){
       fprintf(stderr, "[JobScheduler] Thread initiation error\n");
       return -1;
 	  }
@@ -32,18 +37,23 @@ void* do_work(void* object){
   JobScheduler* sch = reinterpret_cast<JobScheduler*>(object);
   Job* j;
 	while(1){
-		pthread_mutex_lock(&(sch->qlock));  //get the q lock.
+		pthread_mutex_lock(&(sch->qlock));
 
-		while(sch->q->size == 0){ //queue empty
+		while(sch->q->size <= 0){ //while queue empty
+      if(sch->wait_all){
+				pthread_mutex_unlock(&(sch->qlock));
+        pthread_barrier_wait(&(sch->barrier));
+			}
       if(sch->quit){
 				pthread_mutex_unlock(&(sch->qlock));
 				pthread_exit(NULL);
 			}
 			//wait until the condition says queue non-emtpy.
-			pthread_cond_wait(&(sch->q_not_empty),&(sch->qlock));
+      if(sch->q->size <= 0)
+			  pthread_cond_wait(&(sch->q_not_empty),&(sch->qlock));
 		}
 		j = sch->q->head;	//set the job variable j.
-		sch->q->size--;		//decrement the size.
+		sch->q->size--;		//decrement queue size.
 
 		if(sch->q->size == 0){
 			sch->q->head = NULL;
@@ -55,7 +65,6 @@ void* do_work(void* object){
 		}
 		pthread_mutex_unlock(&(sch->qlock));
 		(j->routine) (j->arg); //start routine
-    //barrier await
 		delete j;
   }
 }
@@ -85,21 +94,33 @@ int JobScheduler::wait_all_tasks_finish(){
   while(q->size > 0){ //wait until the queue is empty.
 		pthread_cond_wait(&q_empty,&qlock);
 	}
+  this->wait_all = true;
+  pthread_cond_broadcast(&q_not_empty);
   pthread_mutex_unlock(&qlock);
+
+  pthread_barrier_wait(&barrier);
+  pthread_mutex_lock(&qlock);
+  this->wait_all = false;
+  pthread_mutex_unlock(&qlock);
+
   return 0;
 }
 //-----------------------------------------------------------------------
 int JobScheduler::destroy_scheduler(){
-  wait_all_tasks_finish();
+  pthread_mutex_lock(&qlock);
+  while(q->size > 0){ //wait until the queue is empty.
+		pthread_cond_wait(&q_empty,&qlock);
+	}
   this->quit = true;
   pthread_cond_broadcast(&q_not_empty);
+  pthread_mutex_unlock(&qlock);
   for(int i=0; i<execution_threads; i++){
 		pthread_join(tids[i], NULL);
 	}
-
   pthread_mutex_destroy(&qlock);
 	pthread_cond_destroy(&q_empty);
-	pthread_cond_destroy(&q_not_empty);
+  pthread_cond_destroy(&q_not_empty);
+  pthread_barrier_destroy(&barrier);
   delete this->q;
   delete[] this->tids;
   delete this;
--- src/Multithreading/JobScheduler.h
+++ src/Multithreading/JobScheduler.h
@@ -12,10 +12,12 @@ private:
   pthread_t* tids; // execution threads
 public:
   bool quit;
+  bool wait_all;
   Queue* q; // a queue that holds submitted jobs / tasks
   pthread_mutex_t qlock;		//lock on the queue list
 	pthread_cond_t q_not_empty;	 //non empty and empty condidtion variables
 	pthread_cond_t q_empty;
+  pthread_barrier_t barrier;
   int initialize_scheduler(int);
   int submit_job(Job*);
   int execute_all_jobs();
